ce53e7f0125fe855fbc60c68d64399831542ed9f
danielsf <scott.f.daniel@gmail.com>
2014-09-08 16:03:10 -0700

    small diagnostic change

--- aps.cpp
+++ aps.cpp
@@ -2702,7 +2702,7 @@ void aps::assess_node(int dex){
     }
     
     if(use_it==1 || used_because_distance==1){
-        printf("accepting node %d %d\n",use_it,used_because_distance);
+        printf("accepting node %d %d -- %e\n",use_it,used_because_distance,gg.get_fn(dex));
         nodes.add(dex,dice,&ggWrap);
         i=nodes.get_dim()-1;
         if(iclosest>=0){
