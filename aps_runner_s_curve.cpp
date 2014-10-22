13307a8236b20a38721351b044da11301c87e795
danielsf <scott.f.daniel@gmail.com>
2014-10-22 11:29:41 -0700

    only run for specified number of samples

--- aps_runner_s_curve.cpp
+++ aps_runner_s_curve.cpp
@@ -96,7 +96,8 @@ i=-1;
 //search parameter space until the
 //chisquared function has been called
 //10000 times
-while(aps_test.get_called()<nsamples || aps_test.get_n_active_nodes()>0){
+//while(aps_test.get_called()<nsamples || aps_test.get_n_active_nodes()>0){
+while(aps_test.get_called()<nsamples){
     aps_test.search();    
 }
 aps_test.write_pts();
